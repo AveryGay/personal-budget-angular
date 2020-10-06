2cf77a0f996791f12969528cd5b59078ef70b7af
Avery Gay <agay12@uncc.edu>
2020-10-06 15:31:56 -0400

    video 1 done

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
+// Budget API
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4000;
+const budget = require('/Users/17046/Desktop/VS/week03/personal-budget/budget.json');
 
-app.use('/', express.static('public'));
-
-const budget = require('C:\\Users\\17046\\Desktop\\VS\\week03\\personal-budget\\budget.json');
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(cors());
 
 app.get('/budget', (req, res) => {
-    
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`API served at http://localhost:${port}`)
 });
